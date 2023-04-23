0f056a74d8fe790006243be6466ae2adb8585269
Prashant <prashantd049@gmail.com>
2023-04-23 11:42:29 +0530

    Commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,20 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, useAddress, useContract, useContractRead} from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return (
+  const address= useAddress();
+  const contractAddress= "0x40F8aF704563F871bf7d74a7fc442EEBb1b36b0a" ;
+  const {contract} = useContract(contractAddress);
+  const { data: proposal, isLoading: proposalLoading} = useContractRead(contract,"proposal", 0);
+  const { data: hasvoted, isLoading: hasVotedLoading} = useContractRead(contract, "hasVoted", 0, address);
+    return ( 
     <div className={styles.container}>
       <main className={styles.main}>
-        
+        <div className={styles.card}>
+<h1>Voting Application</h1>
+
+        </div>
       </main>
     </div>
   );
